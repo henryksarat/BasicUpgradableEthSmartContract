0c63558f57de5fa835b3cb0ab81a81d811f85c82
Henryk Sarat <henksinclair10x@gmail.com>
2024-04-01 15:10:42 -0400

    Fixed unit test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Count/i);
+  const linkElement = screen.getByText(/Value Stored/i);
   expect(linkElement).toBeInTheDocument();
 });
